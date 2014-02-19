d9f59812e37b23db50c7cdc8890a5294cd8629b8
vmora <vincent.mora@oslandia.com>
2014-02-19 09:16:31 +0100

    improved presentation of test project

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from qgis.core import *
+
 from pylab import *
 
 import os
@@ -17,7 +18,6 @@ class Gui:
     def __init__(self, iface):
         self.iface = iface
         self.actions = []
-        pass
     
     def initGui(self):
 
@@ -36,6 +36,8 @@ class Gui:
         Processing.addProvider(self.epanetAlgoProvider, True)
         self.spatialiteAlgorithmProvider = SpatialiteAlgorithmProvider()
         Processing.addProvider(self.spatialiteAlgorithmProvider, True)
+
+        QgsMapLayerRegistry.instance().layersAdded.connect( self.layerAdded )
     
     def unload(self):
         Processing.removeProvider(self.spatialiteAlgorithmProvider)
@@ -50,27 +52,37 @@ class Gui:
         if layer.name().lower() == 'reservoirs' or layer.name().lower() == 'tanks':
             res = QgsMapLayerRegistry.instance().mapLayersByName('Node output table')
             if res: 
+                print res
                 assert(len(res) == 1)
-                fig,p = subplots(1,len(layer.selectedFeatures()))
+                nbfeat = len(layer.selectedFeatures())
+                fig, p = None,[]
+                if nbfeat >= 1 : fig,p = subplots(1,nbfeat)
+                if nbfeat == 1 : p = [p]
+                print nbfeat, p
                 for i,s in enumerate(layer.selectedFeatures()):
                     x,y = [],[]
                     for f in res[0].getFeatures(QgsFeatureRequest(QgsExpression("Node = '"+s['ID Noeud']+"'"))):
                         #print s['ID Noeud'],' ',f['Time'],' ',f['Head']
                         t = f['Time'].split(' ')[1].split(':')
                         x.append(int(t[0])*60+int(t[1]))
-                        y.append(f['Head'])
+                        y.append(f['Pressure'])
+                    p[i].axhspan(float(s['Niv Min']), float(s['Niv Max']), color='lightgreen')
+                    p[i].text(0, float(s['Niv Max']), ' overflow')
+                    p[i].text(0, float(s['Niv Min']), ' empty')
+                    p[i].set_axis_bgcolor('r')
                     p[i].plot(x,y)
                     p[i].set_title(layer.name()+' '+s['ID Noeud'])
                     p[i].set_xlabel('Time [min]')
-                    p[i].set_ylabel('Head [m]')
-                show()
+                    p[i].set_ylabel('Level [m]')
+                if fig : show()
 
         if layer.name().lower() == 'pumps':
             res = QgsMapLayerRegistry.instance().mapLayersByName('Link output table')
             if res: 
                 assert(len(res) == 1)
                 nbfeat = len(layer.selectedFeatures())
-                fig, p = subplots(1,nbfeat)
+                fig, p = None,[]
+                if nbfeat >= 1 : fig,p = subplots(1,nbfeat)
                 if nbfeat == 1 : p = [p]
                 for i,s in enumerate(layer.selectedFeatures()):
                     x,y = [],[]
@@ -83,14 +95,36 @@ class Gui:
                     p[i].set_title(layer.name()+' '+s['ID Arc'])
                     p[i].set_xlabel('Time [min]')
                     p[i].set_ylabel('Flow [m3/h]')
-                show()
+                if fig : show()
+
+        if layer.name().lower() == 'pipes':
+            res = QgsMapLayerRegistry.instance().mapLayersByName('Link output table')
+            if res: 
+                assert(len(res) == 1)
+                nbfeat = len(layer.selectedFeatures())
+                fig, p = None,[]
+                if nbfeat >= 1 : fig,p = subplots(1,nbfeat)
+                if nbfeat == 1 : p = [p]
+                for i,s in enumerate(layer.selectedFeatures()):
+                    x,y = [],[]
+                    for f in res[0].getFeatures(QgsFeatureRequest(QgsExpression("Link = '"+s['ID Arc']+"'"))):
+                        #print s['ID Noeud'],' ',f['Time'],' ',f['Head']
+                        t = f['Time'].split(' ')[1].split(':')
+                        x.append(int(t[0])*60+int(t[1]))
+                        y.append(f['Velocity'])
+                    p[i].plot(x,y)
+                    p[i].set_title(layer.name()+' '+s['ID Arc'])
+                    p[i].set_xlabel('Time [min]')
+                    p[i].set_ylabel('Velocity [m/s]')
+                if fig : show()
 
         if layer.name().lower() == 'junctions':
             res = QgsMapLayerRegistry.instance().mapLayersByName('Node output table')
             if res: 
                 assert(len(res) == 1)
                 nbfeat = len(layer.selectedFeatures())
-                fig, p = subplots(1,nbfeat)
+                fig, p = None,[]
+                if nbfeat >= 1 : fig,p = subplots(1,nbfeat)
                 if nbfeat == 1 : p = [p]
                 for i,s in enumerate(layer.selectedFeatures()):
                     x,y = [],[]
@@ -103,7 +137,51 @@ class Gui:
                     p[i].set_title(layer.name()+' '+s['ID Noeud'])
                     p[i].set_xlabel('Time [min]')
                     p[i].set_ylabel('Pressure [m]')
-                show()
+                if fig : show()
 
+    def layerAdded(self, layers):
+        print 'layers added ', layers
 
-        pass
+        # get a map from layer names
+        epanet_layers = {}
+        for name,l in QgsMapLayerRegistry.instance().mapLayers().iteritems():
+            short_name = l.name().lower()
+            if short_name in ['tanks','sources','reservoirs','pumps','pipes','junctions','valves','emitters']:
+                epanet_layers[short_name] = l
+
+        for l in layers :
+            if l.type() != QgsMapLayer.VectorLayer : continue 
+            result_type = None
+            fields = [f.name() for f in l.pendingFields()]
+            if 'Link' in fields and 'AvgVelocity' in fields:
+                result_type = 'Link time agregates'
+            if 'Node' in fields and 'MaxPressure' in fields:
+                result_type = 'Node time agregates'
+            print result_type
+            if result_type == 'Node time agregates':
+                # create join on first field
+                print 'create join on junctions'
+                join_info = QgsVectorJoinInfo()
+                join_info.targetFieldName = 'ID Noeud'
+                join_info.targetFieldIndex = 0
+                join_info.joinLayerId = l.id() 
+                join_info.joinFieldName = 'Node'
+                join_info.joinFieldIndex = 0
+                join_info.memoryCache = False
+                #join_info.cachedAttributes = 
+                for name in ['junctions','emitters','tanks','sources','reservoirs']:
+                    if name in epanet_layers: epanet_layers[name].addJoin( join_info )
+            if result_type == 'Link time agregates':
+                # create join on first field
+                print 'create join on junctions'
+                join_info = QgsVectorJoinInfo()
+                join_info.targetFieldName = 'ID Arc'
+                join_info.targetFieldIndex = 0
+                join_info.joinLayerId = l.id() 
+                join_info.joinFieldName = 'Link'
+                join_info.joinFieldIndex = 0
+                join_info.memoryCache = False
+                #join_info.cachedAttributes = 
+                for name in ['pipes','valves','pumps']:
+                    if name in epanet_layers: epanet_layers[name].addJoin( join_info )
+
